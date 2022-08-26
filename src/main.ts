86205032aaf7306c40877fe2b3182682aeacd635
OKY <okyaneka@gmail.com>
2022-08-26 08:03:34 +0700

    init ui framework

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import './styles/index.scss'
+
+// If you want to use ElMessage, import it.
+import 'element-plus/theme-chalk/src/message.scss'
 
 const app = createApp(App)
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,41 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+// Element Plus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      dts: 'src/autoimport.d.ts',
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      dts: 'src/components.d.ts',
+      extensions: ['vue', 'md'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass',
+        }),
+      ],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      },
+    },
+  },
 })
