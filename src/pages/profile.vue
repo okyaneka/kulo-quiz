672a1143a1bb70e539f5955a124e4fb62a746b0f
OKY <okyaneka@gmail.com>
2022-10-09 21:16:03 +0700

    adjust: padding profile

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -32,43 +32,39 @@ meta:
 </script>
 
 <template>
-  <el-row style="padding: 20px 0">
-    <el-col>
-      <el-card>
-        <el-row :gutter="16">
-          <el-col :span="8">
-            <div id="avatar" ref="avatar"></div>
-            <el-avatar
-              :src="
-                authUser?.photoURL ??
-                'https://kusonime.com/wp-content/uploads/2022/05/Ao-Ashi-605x340.jpg'
-              "
-              :size="avatarWidth"
-              style="transition: all 0.3s"
-            ></el-avatar>
-          </el-col>
-          <el-col :span="16" style="margin: 0">
-            <h3 style="margin-bottom: 8px">
-              {{ authUser?.displayName ?? '-' }}
-            </h3>
-            <el-button>Edit Profil</el-button>
-            <el-button circle>
-              <template #icon> <svg-icon name="setting-2" /> </template
-            ></el-button>
-          </el-col>
-        </el-row>
-      </el-card>
-    </el-col>
+  <el-card :body-style="{ padding: 0 }">
+    <el-row :gutter="16" style="padding: 20px">
+      <el-col :span="8">
+        <div id="avatar" ref="avatar"></div>
+        <el-avatar
+          :src="
+            authUser?.photoURL ??
+            'https://kusonime.com/wp-content/uploads/2022/05/Ao-Ashi-605x340.jpg'
+          "
+          :size="avatarWidth"
+          style="transition: all 0.3s"
+        ></el-avatar>
+      </el-col>
+      <el-col :span="16" style="margin: 0">
+        <h3 style="margin-bottom: 8px">
+          {{ authUser?.displayName ?? '-' }}
+        </h3>
+        <el-button>Edit Profil</el-button>
+        <el-button circle>
+          <template #icon> <svg-icon name="setting-2" /> </template
+        ></el-button>
+      </el-col>
+    </el-row>
 
-    <el-col>
-      <el-card :body-style="{ padding: '0' }">
+    <el-row>
+      <el-col>
         <el-tabs v-model="active" stretch class="no-margin-tabs">
           <el-tab-pane name="my" label="My Quiz"> </el-tab-pane>
           <el-tab-pane name="all" label="Draft"> </el-tab-pane>
           <el-tab-pane name="s" label="Saved"> </el-tab-pane>
         </el-tabs>
         <router-view />
-      </el-card>
-    </el-col>
-  </el-row>
+      </el-col>
+    </el-row>
+  </el-card>
 </template>
