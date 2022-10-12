d05f102ce38de1e9c735675a86544b3d8c929652
OKY <okyaneka@gmail.com>
2022-10-12 07:11:12 +0700

    adjust: question form ux touch friendly

--- src/composables/components/question-form/question-form.vue
+++ src/composables/components/question-form/question-form.vue
@@ -33,7 +33,7 @@
     return toFormValidator(z.object(validator))
   })
 
-  const { values, errors, setValues, validate } = useForm<
+  const { values, errors, setValues, validate, setFieldValue } = useForm<
     Partial<QuestionPayloads>
   >({
     validationSchema: validator,
@@ -62,25 +62,60 @@
 
   onMounted(() => {
     setValues(props.value)
+    if (props.value.point != undefined)
+      quickPoint.value = quickValues.includes(props.value.point as number)
+        ? props.value.point
+        : 'custom'
+    if (props.value.timer != undefined)
+      quickTimer.value = quickValues.includes(props.value.timer as number)
+        ? props.value.timer
+        : 'custom'
+  })
+
+  const quickValues = [5, 10, 20, 50, 100, 'custom']
+  const quickPoint = ref<string | number>()
+  const quickTimer = ref<string | number | null>()
+  watch(quickPoint, (value) => {
+    if (value && typeof value == 'number') setFieldValue('point', value)
+  })
+  watch(quickTimer, (value) => {
+    if (value && typeof value == 'number') setFieldValue('timer', value)
   })
 </script>
 
 <template>
   <el-form label-position="top" :disabled="disabled">
     <el-form-item :error="errors.point" label="Point">
-      <el-input-number v-model.number="values.point"></el-input-number>
+      <el-row justify="space-between" style="width: 100%">
+        <el-button
+          v-for="(point, i) in quickValues"
+          :type="quickPoint == point ? 'primary' : undefined"
+          :key="`point-${i}`"
+          round
+          @click="quickPoint = point"
+          >{{ point }}</el-button
+        >
+      </el-row>
+
+      <el-input
+        v-if="quickPoint == 'custom'"
+        v-model.number="values.point"
+        style="margin-top: 12px"
+        placeholder="Input point"
+      ></el-input>
     </el-form-item>
 
     <el-form-item :error="errors.mode" label="Question mode">
-      <el-select v-model="values.mode" style="width: 100%">
-        <template v-for="(option, i) in QuestionMode" :key="`option-${i}`">
-          <el-option
-            v-if="isNaN(option)"
-            :label="option"
-            :value="QuestionMode[option]"
-          />
-        </template>
-      </el-select>
+      <template v-for="(option, i) in QuestionMode" :key="`option-${i}`">
+        <el-button
+          v-if="!isNaN(option)"
+          :type="values.mode == option ? 'primary' : undefined"
+          @click="values.mode = option"
+          round
+        >
+          {{ QuestionMode[option] }}
+        </el-button>
+      </template>
     </el-form-item>
 
     <el-form-item :error="errors.guide" label="Question guide">
@@ -96,9 +131,24 @@
       </el-col>
     </el-form-item>
 
-    <el-form-item :error="errors.timer" label="Timer">
-      <el-input v-model.number="values.timer" type="number">
-        <template #append>second</template>
+    <el-form-item :error="errors.timer" label="Timer (seconds)">
+      <el-row justify="space-between" style="width: 100%">
+        <el-button
+          v-for="(point, i) in quickValues"
+          :type="quickTimer == point ? 'primary' : undefined"
+          :key="`point-${i}`"
+          round
+          @click="quickTimer = point"
+          >{{ point }}</el-button
+        >
+      </el-row>
+
+      <el-input
+        v-if="quickTimer == 'custom'"
+        v-model.number="values.timer"
+        placeholder="Input timer"
+        style="margin-top: 12px"
+      >
       </el-input>
     </el-form-item>
 
